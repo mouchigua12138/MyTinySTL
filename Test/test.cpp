d8aa6b4137903f2ebe6fca608b43bf9a79312e57
MrChen <1114990552@qq.com>
2023-10-20 17:45:56 +0800

    更新vs工程&格式编辑

--- Test/test.cpp
+++ Test/test.cpp
@@ -21,32 +21,32 @@
 #include "unordered_set_test.h"
 #include "string_test.h"
 
-int main()
-{
-  using namespace mystl::test;
+int main() {
 
-  std::cout.sync_with_stdio(false);
+    using namespace mystl::test;
 
-  RUN_ALL_TESTS();
-  algorithm_performance_test::algorithm_performance_test();
-  vector_test::vector_test();
-  list_test::list_test();
-  deque_test::deque_test();
-  queue_test::queue_test();
-  queue_test::priority_test();
-  stack_test::stack_test();
-  map_test::map_test();
-  map_test::multimap_test();
-  set_test::set_test();
-  set_test::multiset_test();
-  unordered_map_test::unordered_map_test();
-  unordered_map_test::unordered_multimap_test();
-  unordered_set_test::unordered_set_test();
-  unordered_set_test::unordered_multiset_test();
-  string_test::string_test();
+    std::cout.sync_with_stdio(false);
+
+    RUN_ALL_TESTS();
+    algorithm_performance_test::algorithm_performance_test();
+    vector_test::vector_test();
+    list_test::list_test();
+    deque_test::deque_test();
+    queue_test::queue_test();
+    queue_test::priority_test();
+    stack_test::stack_test();
+    map_test::map_test();
+    map_test::multimap_test();
+    set_test::set_test();
+    set_test::multiset_test();
+    unordered_map_test::unordered_map_test();
+    unordered_map_test::unordered_multimap_test();
+    unordered_set_test::unordered_set_test();
+    unordered_set_test::unordered_multiset_test();
+    string_test::string_test();
 
 #if defined(_MSC_VER) && defined(_DEBUG)
-  _CrtDumpMemoryLeaks();
+    _CrtDumpMemoryLeaks();
 #endif // check memory leaks
 
 }
